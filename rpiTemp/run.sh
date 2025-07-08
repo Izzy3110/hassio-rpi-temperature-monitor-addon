1715ec6989dd9f7647189367e8969167a52f87e9
Sascha Frank <sfrank@wyl-online.de>
2025-07-08 16:55:34 +0200

    debugging percentage as it stays at 0%

--- rpiTemp/run.sh
+++ rpiTemp/run.sh
@@ -22,8 +22,15 @@ until false; do
     cpuTemp=$(( ( $cpuTemp *  9/5 ) + 32 ));
     unit="F"
   fi
+
   echo "Current CPU Temperature $cpuTemp °$unit"
   echo "Current Mem Free        $memFree $memUnit"
+  echo "Current Mem Free (perc) $memFreePercent $memUnitPercentage"
+  echo "Raw Values:"
+  echo " - free                 $memRawFree Kb"
+  echo " - total                $memRawTotal Kb"
+  echo ""
+
   curl -s -X POST -H "Content-Type: application/json"  -H "Authorization: Bearer $HASSIO_TOKEN" -d '{"state": "'$cpuTemp'", "attributes":  {"unit_of_measurement": "°'$unit'", "icon": "mdi:temperature-celsius", "friendly_name": "CPU Temperature"}}' http://hassio/homeassistant/api/states/sensor.cpu_temperature 2>/dev/null
   curl -s -X POST -H "Content-Type: application/json"  -H "Authorization: Bearer $HASSIO_TOKEN" -d '{"state": "'$memFree'", "attributes":  {"unit_of_measurement": "'$memUnit'", "icon": "mdi:memory", "friendly_name": "Memory Free"}}' http://hassio/homeassistant/api/states/sensor.mem_free 2>/dev/null
   curl -s -X POST -H "Content-Type: application/json"  -H "Authorization: Bearer $HASSIO_TOKEN" -d '{"state": "'$memFreePercent'", "attributes":  {"unit_of_measurement": "'$memUnitPercentage'", "icon": "mdi:memory", "friendly_name": "Memory Free - Percentage"}}' http://hassio/homeassistant/api/states/sensor.mem_free_percentage 2>/dev/null
